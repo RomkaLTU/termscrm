2c23da78b47672f009cb8844e201f13625587e04
RD <hello@single5.com>
2019-05-30 17:35:57 +0300

    clean up

--- resources/views/contracts/edit.blade.php
+++ resources/views/contracts/edit.blade.php
@@ -49,18 +49,5 @@
             </div>
         </div>
     </form>
-
-    <div class="kt-portlet kt-portlet--mobile">
-        <div class="kt-portlet__head">
-            <div class="kt-portlet__head-label">
-                <h3 class="kt-portlet__head-title">
-                    {{ __('Sutarties objektai') }}
-                </h3>
-            </div>
-        </div>
-        <div class="kt-portlet__body">
-
-        </div>
-    </div>
 </div>
 @endsection
