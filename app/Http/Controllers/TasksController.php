f84d184ba7896224102008b7db0ec4e6ac73bb17
RD <romualdas@prodev.lt>
2019-07-17 10:26:26 +0300

    PDF css fix, TrustProxies fix.

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -290,7 +290,7 @@ class TasksController extends Controller
         <head>
             <meta charset="utf-8">
             <title>Terminų valdymo sistema - UAB Ekometrija</title>
-            <link href="https://terms.srv1.single5.com/css/tailwind.css" rel="stylesheet" type="text/css">
+            <link href="'. env('APP_URL') .'/css/tailwind.css" rel="stylesheet" type="text/css">
             <style>
                 body {
                     font-family: DejaVu Sans, sans-serif;
